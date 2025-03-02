714bedd3c7bd6feac9f5b980977983a70d2c7602
divyam.20scse1010914@galgotiasuniversity.edu.in <divyam.20scse1010914@galgotiasuniversity.edu.in>
2025-03-02 17:49:25 +0530

    Backend

--- Backend/app.js
+++ Backend/app.js
@@ -9,8 +9,8 @@ const PORT = 4000;
 
 mongoose.connect('mongodb://localhost:27017/YogDB').then(() => console.log('Database Connected')).catch(err => console.log('Unable to connect DB',err));
 
-app.use('/',userroutes);
-app.use('/',yogroutes)
+app.use('/users',userroutes);
+app.use('/yogasanas',yogroutes)
 
 app.listen(PORT,() => {
     console.log(`Server Running at ${PORT}`)
